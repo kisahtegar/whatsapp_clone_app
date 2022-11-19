132495d25eea854f0eff9236284a221921b99bbc
kisah <data.kisahtegar@gmail.com>
2022-11-19 12:19:26 +0700

    adding Firebase, UI : Screen, etc

---
+++ lib/presentation/pages/phone_verification_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:whatsapp_clone_app/presentation/pages/set_initial_profile_page.dart';
+import 'package:whatsapp_clone_app/presentation/widgets/theme/style.dart';
+
+class PhoneVerificationPage extends StatefulWidget {
+  const PhoneVerificationPage({super.key});
+
+  @override
+  State<PhoneVerificationPage> createState() => _PhoneVerificationPageState();
+}
+
+class _PhoneVerificationPageState extends State<PhoneVerificationPage> {
+  TextEditingController _pinCodeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Column(
+          children: <Widget>[
+            // NOTE: Top Text and Description
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                Text(""),
+                Text(
+                  "Verify your phone number",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: greenColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Icon(Icons.more_vert)
+              ],
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "WhatsApp Clone will send and SMS message (carrier charges may apply) to verify your phone number. Enter your country code and phone number:",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+
+            // NOTE: Input Text pin code
+            _pinCodeWidget(context),
+
+            // NOTE: Next Button
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: MaterialButton(
+                  color: greenColor,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => const SetInitialProfilePage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "Next",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _pinCodeWidget(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 50),
+      child: Column(
+        children: <Widget>[
+          PinCodeTextField(
+            appContext: context,
+            controller: _pinCodeController,
+            length: 6,
+            backgroundColor: Colors.transparent,
+            obscureText: true,
+            autoDisposeControllers: false,
+            onChanged: (pinCode) {
+              print(pinCode);
+            },
+          ),
+          const Text("Enter your 6 digit code")
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/pages/set_initial_profile_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone_app/presentation/widgets/theme/style.dart';
+
+class SetInitialProfilePage extends StatefulWidget {
+  const SetInitialProfilePage({super.key});
+
+  @override
+  State<SetInitialProfilePage> createState() => _SetInitialProfilePageState();
+}
+
+class _SetInitialProfilePageState extends State<SetInitialProfilePage> {
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+        child: Column(
+          children: <Widget>[
+            // NOTE: Top Text and Description
+            const Text(
+              "Profile Info",
+              style: TextStyle(
+                color: greenColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              "Please provide your name and an optional Profile photo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 30),
+
+            // NOTE: Row image, name, etc.
+            _rowWidget(),
+
+            // NOTE: Next Button
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: MaterialButton(
+                  color: greenColor,
+                  onPressed: () {},
+                  child: const Text(
+                    "Next",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _rowWidget() {
+    return SizedBox(
+      child: Row(
+        children: <Widget>[
+          // Image Picker
+          Container(
+            width: 50,
+            height: 50,
+            decoration: BoxDecoration(
+              color: textIconColorGray,
+              borderRadius: const BorderRadius.all(Radius.circular(25)),
+            ),
+            child: const Icon(Icons.camera_alt),
+          ),
+          const SizedBox(width: 8),
+
+          // Name
+          Expanded(
+            child: TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                hintText: "Enter your name",
+              ),
+            ),
+          ),
+          const SizedBox(width: 8.0),
+
+          // Emoticon
+          Container(
+            width: 35,
+            height: 35,
+            decoration: BoxDecoration(
+              color: textIconColorGray,
+              borderRadius: const BorderRadius.all(Radius.circular(25)),
+            ),
+            child: const Icon(Icons.insert_emoticon),
+          ),
+        ],
+      ),
+    );
+  }
+}
